b69834b226dc4303647147807adbe509043805d4
AlymiReyna <alymireyna22@gmail.com>
2022-04-27 17:44:48 -0500

    Add : Pruebas de unidad

---
+++ index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+const alymi = new MissionCommander("Alymi")
+const leo = new MissionCommander("Leo")
+console.log(carlo.name)
+console.log(alymi.name)
+console.log(leo.name)
